1d0e69f538004dcc8c283fc80c1f4fa790713c08
dagyrox <dagyrox@gmail.com>
2017-03-20 13:03:26 -0400

    empty term bug fix

--- routes/crypt.js
+++ routes/crypt.js
@@ -2,19 +2,27 @@ var crypto = require('crypto');
 var algorithm = 'aes-256-ctr';
 var defaultKey = '<your default password here>';
 
-exports.encrypt = function(text, key){
+exports.encrypt = function(term, key){
+    if(!term){
+        return;
+    }
+
     var _key = key || defaultKey;
     var cipher = crypto.createCipher(algorithm, _key);
-    var crypted = cipher.update(text,'utf8','hex');
+    var crypted = cipher.update(term,'utf8','hex');
     crypted += cipher.final('hex');
 
     return crypted;
 }
 
-exports.decrypt = function(text, key){
+exports.decrypt = function(term, key){
+    if(!term){
+        return;
+    }
+    
     var _key = key || defaultKey;
     var decipher = crypto.createDecipher(algorithm, _key);
-    var dec = decipher.update(text,'hex','utf8');
+    var dec = decipher.update(term,'hex','utf8');
     dec += decipher.final('utf8');
 
     return dec;
